b7c528c526031244e8ae09e3e6fa4f1822130dfa
EC2 Default User <ec2-user@ip-172-31-23-146.ec2.internal>
2021-09-24 14:44:22 +0000

    [Add]2.10.rb 2.10.2

--- 2.10.rb
+++ 2.10.rb
@@ -1,3 +1,4 @@
+# && || の戻り値を評価するタイミング
 puts 1 && 2 && 3
 puts 1 && nil && 3
 puts 1 && false && 3
@@ -7,3 +8,18 @@ puts false && nil
 puts nil || false
 puts false || nil
 puts nil || false || 2 || 3
+
+# ! && || not and or  の優先順位
+t1 = true
+t2 = true
+f1 = false
+f2 = false
+
+puts !f1 && f2
+puts !(f1 && f2)
+
+puts (not f1 && f2)
+puts not(f1 && f2)
+
+puts t1 || t2 && f1
+puts t1 or t2 and f1
