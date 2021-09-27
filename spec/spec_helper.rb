c2205d4a10fcbaf1216e99f48bab4a1382fd0c0f
yanmasa <ueda3ban@gmail.com>
2021-09-27 07:38:38 +0000

    [Add]class ConvertHashSyntaxTest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,16 +99,19 @@ RSpec.configure do |config|
 =end
 end
 
-require './3.0/FizzBuzz'
+# require './3.0/FizzBuzz'
 
-RSpec.describe 'Fizz Buzz' do
-  it 'FizzBuzz' do
-    expect(fizz_buzz(1)).to eq '1'
-    expect(fizz_buzz(2)).to eq '2'
-    expect(fizz_buzz(3)).to eq 'Fizz'
-    expect(fizz_buzz(4)).to eq '4'
-    expect(fizz_buzz(5)).to eq 'Buzz'
-    expect(fizz_buzz(6)).to eq 'Fizz'
-    expect(fizz_buzz(15)).to eq 'FizzBuzz'
-  end
-end
+# RSpec.describe 'Fizz Buzz' do
+#   it 'FizzBuzz' do
+#     expect(fizz_buzz(1)).to eq '1'
+#     expect(fizz_buzz(2)).to eq '2'
+#     expect(fizz_buzz(3)).to eq 'Fizz'
+#     expect(fizz_buzz(4)).to eq '4'
+#     expect(fizz_buzz(5)).to eq 'Buzz'
+#     expect(fizz_buzz(6)).to eq 'Fizz'
+#     expect(fizz_buzz(15)).to eq 'FizzBuzz'
+#   end
+# end
+
+
+
